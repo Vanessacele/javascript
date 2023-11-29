61318a48752152805289f4e64c911b1eabcfeaff
vanessa celestrini <vanessa.celestrini@aluno.senai.br>
2023-11-28 21:19:18 -0300

    mensagem do commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// definindo um array de alunos
 let alunos = [
     {
         nome: "Aninhha",
@@ -21,7 +20,7 @@ window.onload = (event) => {
     let tbody = document.querySelector("#myTable");
     alunos.forEach((aluno, index) => {
         let tr = document.createElement("tr");
-
+        tr.id = index + 1;
         let tdCod = document.createElement("td");
         let tdNome = document.createElement("td");
         let tdEmail = document.createElement("td");
@@ -51,9 +50,54 @@ window.onload = (event) => {
 
 }
 
-function remover (id) {
+function remover(id) {
     let row = id.parentNode.parentNode.id;
     row = document.getElementById(row);
-    row.parentNode.remove
+    row.parentNode.removeChild(row);
+    return false
+}
+
+function adicionar() {
+      //Definindo as variaveis e recebendo os dados
+      let nome = document.getElementById('nome').value;
+      let email =document.getElementById('email').value;
+      let telefone =document.getElementById('telefone').value;
+      let profissao = document.getElementById('profissao').value;
+      let table = document.getElementById('myTable');
+  
+      //Calculando o tamanho da Tabela
+      let tableSize = table.rows.lenght;
+  
+      //Inserindo uma linha abaixo da Tabela
+      let row = table.insertRow(tableSize);
+      let col1 = row.insertCell(0);
+      let col2 = row.insertCell(1);
+      let col3 = row.insertCell(2);
+      let col4 = row.insertCell(3);
+      let col5 = row.insertCell(4);
+      let col6 = row.insertCell(5);
+  
+       //Adicionando o id no elemento a ser criado
+      row.id = tableSize;
+  
+      //Criando o codigo do botão para remover a linha
+      let btnCode = "<button class='remove-btn' onclick='remover(this)'>Remover</button>";
+      //Preenchendo as celulas da linha
+      col1.innerHTML = tableSize;
+      col2.innerHTML = nome;
+      col3.innerHTML = email;
+      col4.innerHTML = telefone;
+      col5.innerHTML = profissao;
+      col6.innerHTML = btnCode;
+  
+   //Limpando os campos de inserção de dados
+      document.getElementById('nome').value = "";
+      document.getElementById('email').value = "";
+      document.getElementById('telefone').value = "";
+      document.getElementById('profissao').value = "";
+  
+  //Retornando 'false' para impedir o reload da pagina
+      return false
+  
 
 }
